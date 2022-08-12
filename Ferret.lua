29e696924be448c7fdfaf305f0be3d7f151e625a
C4THacks <111158757+C4THacks@users.noreply.github.com>
2022-08-12 19:33:52 -0400

    Update Ferret.lua

--- Ferret.lua
+++ Ferret.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
-local Window = Library.CreateLib("Ferret Hub | Da Hood", "Ocean")
+local Window = Library.CreateLib("Ferret Hub | Nico's Nextbots")
 
 local Player = game.Players.LocalPlayer
 local Character = Player.Character
@@ -11,13 +11,19 @@ local MovementSection = Movement:NewSection("OP Stuff")
 local TPS = Window:NewTab("TPS")
 local TPSection = TPS:NewSection("TPSection")
 
-MovementSection:NewButton("Speed [X]", "OP Module", function()
+local FunTab = Window:NewTab("Fun")
+local FSection = FunTab:NewSection("Scary")
+
+local VersionsTab = Window:NewTab("Versions")
+local VSection = VersionsTab:NewSection("Versions")
+
+MovementSection:NewButton("Speed [Z]", "OP Module", function()
    
    local userInput = game:service('UserInputService')
             local runService = game:service('RunService')
             
             userInput.InputBegan:connect(function(Key)
-                if Key.KeyCode == Enum.KeyCode.X then
+                if Key.KeyCode == Enum.KeyCode.Z then
                     Enabled = not Enabled
                     if Enabled == true then
                         repeat
@@ -30,15 +36,41 @@ MovementSection:NewButton("Speed [X]", "OP Module", function()
    
 end)
 
-MovementSection:NewTextBox("Speed Multiplier", " ", function(value)
-    getgenv().Multiplier = value / 2
+
+MovementSection:NewTextBox("Speed Multiplier", " ", function(txt)
+    getgenv().Multiplier = txt
 end)
 
-local Choices = {
-    "Iphone Shop"
-}
+MovementSection:NewLabel("1-3 - Recommended Speed.")
 
-TPSection:NewButton("I-Phone Store", " ", function()
-    Player.Character.HumanoidRootPart.CFrame = CFrame.new(-105, 22, -871)
-    Player.Character.HumanoidRootPart.CFrame = Player.Character.HumanoidRootPart.CFrame * CFrame.Angles(0, 90, 0)
+MovementSection:NewToggle("Auto Sprint", " ", function(state)
+    if state then
+        Character.scripts.running.Value = true
+        game.StarterPlayer.StarterPlayerScripts.options.runtoggle.Value = false
+    else
+        Character.scripts.running.Value = false
+    end
+end)
+
+TPSection:NewButton("Garage Floor 1 ", " ", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(275, 34, 278)
+end)
+
+
+TPSection:NewButton("Garage Floor 2 ", " ", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(252, 57, 279)
+end)
+
+
+
+TPSection:NewButton("Garage Floor 3 ", " ", function()
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(260, 80, 277)
+end)
+
+FSection:NewButton("Disable Ragdoll", " ", function()
+    game.StarterPlayer.StarterCharacterScripts.scripts.ragdoll.Enabled = false
+end)
+
+VSection:NewButton("Da Hood Version", "The Da Hood version of Ferret Hub.", function()
+    local DHWindow = Library.CreateLib("Ferret Hub | Da Hood", "BloodTheme")
 end)
